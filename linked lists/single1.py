0da15e0e154c66ab7f51ca3709427a1bc016c0cc
MMM-BOUH <medm.bouh@gmail.com>
2024-01-11 10:26:24 +0900

    added traverse method

--- linked lists/single1.py
+++ linked lists/single1.py
@@ -20,11 +20,13 @@ class LinkedList:
             self.head = new_node
             return
         current = self.head
-        print("Current: ", current)
         while current.next:
-            print("current.next", current.next)
             current = current.next
-        current.next = new_node # in python for example,
+        current.next = new_node 
+        print("here is the new_node", new_node)
+        print("Here is current.next", current.next)
+        
+        # in python for example,
         # the next variabe is just another node(has data and next) that points to an 
         # object(another node), but in C, C++ a pointer is an address in the memory.
          # Conclusion of the append method in a singly linked list
@@ -32,11 +34,21 @@ class LinkedList:
          # after the second we'll tell the program that our first node points to the second node
          # and at the third iteration we'll tell our program that the second node points to the third one 
          # and finally the third node will points to null, and this is how we created a linked, where we know the first node(head) and the last node that points to null.
-         
+         # زبدة الموضوع 
+         # Yes, you've understood it correctly. In the context of linked lists, "adding a new node" is indeed about making the previous last node point to the new node. 
+         # This might be a bit different from what you'd expect, especially if you're comparing it to adding items to an array or a list in a more traditional sense.
+    
+    def traverse(self):
+        current = self.head
+        while current:
+            print(current.data)
+            current = current.next
+        
 linked_list = LinkedList()
 linked_list.append(1)
 linked_list.append(2)
 linked_list.append(3)
+linked_list.traverse()
 # linked_list.append(4)
 
 
