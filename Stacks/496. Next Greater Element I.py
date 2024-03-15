97947c8498447f681df92391409fd05a0797cf1e
MMM-BOUH <medm.bouh@gmail.com>
2024-03-15 12:14:11 +0900

    solved 496. Next Greater Element I

--- Stacks/496. Next Greater Element I.py
+++ Stacks/496. Next Greater Element I.py
@@ -11,4 +11,23 @@ class Solution:
         for num in stack:
             next_greater[num] = -1
         result = [next_greater[num] for num in nums1]
-        
+'''brute force approach
+def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+output = []
+for i in range(len(nums1)):
+found = False
+for j in range(len(nums2)):
+if nums1[i] == nums2[j]:
+    if j == len(nums2)-1:
+        output.append(-1)
+        continue
+    else:
+        for k in range(j+1, len(nums2)):
+            if nums2[k] > nums2[j]:
+                output.append(nums2[k]) 
+                found = True
+                break
+        if not found:
+            output.append(-1)
+return output
+'''
