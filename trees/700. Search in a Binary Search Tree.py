a7b582f41b0e6d8c961545e3279e1519da0dad92
MMM-BOUH <medm.bouh@gmail.com>
2024-05-01 12:00:40 +0900

    added a small comment on the process of 700. Search in a Binary Search Tree

--- trees/700. Search in a Binary Search Tree.py
+++ trees/700. Search in a Binary Search Tree.py
@@ -9,6 +9,7 @@ class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         if root is None:
             return None
+        #It means that the function will call itself again and again until there is no val < root.val in the first case, or no val > root.val   in the second case. then it will pass to the third condition if root.val == val: it will return the node with its subnodes if this condition is true.
         if val < root.val:
             return self.searchBST(root.left, val)
         if val > root.val:
